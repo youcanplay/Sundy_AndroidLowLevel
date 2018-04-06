b2cf07e9dd4094b40cffcf4d757082c87d695c3b
youcanplay <380590456@qq.com>
2018-04-06 23:42:10 +0800

    修正了信号量集的问题

--- source/demo_ipc/sem/sem.c
+++ source/demo_ipc/sem/sem.c
@@ -7,6 +7,13 @@ key_t semid;
 
 struct sembuf sem_down = {0, -1, 0};
 struct sembuf sem_up = {0, 1, 0};
+union semun {
+    int val;
+    struct semid_ds *buf;
+    unsigned short *array;
+    struct seminfo *_buf;
+};
+
 
 void down()
 {
@@ -20,14 +27,22 @@ void up()
 
 int main()
 {
-    semid = semget(12345, 1, IPC_CREAT|0666);
-   // union semun arg;
-  //  ushort array[100] = {0};
-//    arg.array = array;
-    //arg.val = 1;
-    if(semctl(semid, 1, SETVAL) == -1) {
+    int ret = -1;
+    union semun arg;
+    arg.val = 1;
+    semid = semget((key_t)22345, 1, IPC_CREAT|0666);
+    if(semid == -1) {
+	perror("semget error");
+	return 1;
+    }	   
+ 
+    ret = semctl(semid, 1, SETVAL, arg);
+    if(ret == -1) {
 	perror("semctl error");
+	return 1;     
     }
+
+
     if(semid != -1) {
 	down();
 	printf("hello world.\n");
